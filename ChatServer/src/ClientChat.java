0b52c4d47a358c280c82ca50bd3b0e287d60292d
RubenMC <rubenmotacasal@gmail.com>
2020-02-27 18:47:47 +0000

    stuff

---
+++ ChatServer/src/ClientChat.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientChat implements Runnable {
+
+    public ClientChat(){
+    }
+
+    public static void main(String[] args){
+
+        try {
+            ClientChat clientChat = new ClientChat();
+            clientChat.createConnection();
+            Thread thread = new Thread(clientChat);
+            thread.start();
+            while(true){
+                clientChat.receiveMessage();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+    }
+
+    private String hostName;
+    private int portNumber;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void createConnection(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter host");
+        hostName = scanner.next();
+        System.out.println("Enter port");
+        portNumber = scanner.nextInt();
+
+        try {
+            clientSocket = new Socket(hostName, portNumber);
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeMessage(){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter message");
+            String message = scanner.next();
+
+            out.write(message);
+            out.flush();
+    }
+
+    public void receiveMessage() throws IOException {
+        System.out.println(in.readLine());
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            writeMessage();
+        }
+    }
+}
---
+++ ChatServer/src/ServerChat.java
@@ -0,0 +1,17 @@
+public class ServerChat {
+
+    public static void main(String[] args){
+
+
+    }
+
+    private class ServerWorker implements Runnable{
+
+
+        @Override
+        public void run() {
+            
+        }
+    }
+
+}
